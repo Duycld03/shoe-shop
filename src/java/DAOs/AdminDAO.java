a3dfd4a7e5d9d312f9ec01444ef1cc4b33b07e85
PhucDoan1123 <phucdt1123@gmail.com>
2024-02-26 19:27:17 +0700

    pull new code

--- src/java/DAOs/AdminDAO.java
+++ src/java/DAOs/AdminDAO.java
@@ -16,29 +16,29 @@ import java.util.logging.Logger;
  * @author Duy
  */
 public class AdminDAO {
-
-	private Connection conn;
-	private PreparedStatement ps;
-	private ResultSet rs;
-
-	public AdminDAO() {
-		conn = DBConnection.getConnection();
-	}
-
-	public Admin checkLogin(String username, String password) {
-		String sql = "select * from Admins where UserName = ? and Password = ?";
-		try {
-			ps = conn.prepareStatement(sql);
-			ps.setString(1, username);
-			ps.setString(2, MD5.getMd5(password));
-			rs = ps.executeQuery();
-			if (rs.next()) {
-				Admin admin = new Admin(rs.getString("AdminID"), rs.getString("UserName"), rs.getString("Password"), rs.getString("Email"), rs.getString("FullName"));
-				return admin;
-			}
-		} catch (SQLException ex) {
-			Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
-		}
-		return null;
-	}
+    
+    private Connection conn;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    
+    public AdminDAO() {
+        conn = DBConnection.getConnection();
+    }
+    
+    public Admin checkLogin(String username, String password) {
+        String sql = "select * from Admins where UserName = ? and Password = ?";
+        try {
+            ps = conn.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, MD5.getMd5(password));
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Admin admin = new Admin(rs.getString("AdminID"), rs.getString("UserName"), rs.getString("Password"), rs.getString("Email"), rs.getString("FullName"), rs.getString("PhoneNumber"));
+                return admin;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(CustomerDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
 }
