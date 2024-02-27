e222cfa6667fc724e3ad52e05ed581c7e84d71bf
Duycld03 <truongduy2003@gmail.com>
2024-02-27 17:50:25 +0700

    fix conflict

--- src/java/Models/Order.java
+++ src/java/Models/Order.java
@@ -8,92 +8,90 @@ import java.sql.Timestamp;
  */
 public class Order {
 
-    private String orderId;
-    private float totalAmount;
-    private Timestamp orderDate;
-    private String PaymentStatus;
-    private String orderStatus;
-    private String customerId;
-    private String methodId;
-    private String StaffID;
+	private String orderId;
+	private float totalAmount;
+	private Timestamp orderDate;
+	private String PaymentStatus;
+	private String orderStatus;
+	private String customerId;
+	private String methodId;
+	private String StaffID;
 
-    public Order() {
-    }
+	public Order() {
+	}
 
-    public Order(String orderId, float totalAmount, Timestamp orderDate, String PaymentStatus, String orderStatus, String customerId, String methodId, String StaffID) {
-        this.orderId = orderId;
-        this.totalAmount = totalAmount;
-        this.orderDate = orderDate;
-        this.PaymentStatus = PaymentStatus;
-        this.orderStatus = orderStatus;
-        this.customerId = customerId;
-        this.methodId = methodId;
-        this.StaffID = StaffID;
-    }
+	public Order(String orderId, float totalAmount, Timestamp orderDate, String PaymentStatus, String orderStatus, String customerId, String methodId) {
+		this.orderId = orderId;
+		this.totalAmount = totalAmount;
+		this.orderDate = orderDate;
+		this.PaymentStatus = PaymentStatus;
+		this.orderStatus = orderStatus;
+		this.customerId = customerId;
+		this.methodId = methodId;
+	}
 
-    public String getOrderId() {
-        return orderId;
-    }
+	public String getOrderId() {
+		return orderId;
+	}
 
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
+	public void setOrderId(String orderId) {
+		this.orderId = orderId;
+	}
 
-    public float getTotalAmount() {
-        return totalAmount;
-    }
+	public float getTotalAmount() {
+		return totalAmount;
+	}
 
-    public void setTotalAmount(float totalAmount) {
-        this.totalAmount = totalAmount;
-    }
+	public void setTotalAmount(float totalAmount) {
+		this.totalAmount = totalAmount;
+	}
 
-    public Timestamp getOrderDate() {
-        return orderDate;
-    }
+	public Timestamp getOrderDate() {
+		return orderDate;
+	}
 
-    public void setOrderDate(Timestamp orderDate) {
-        this.orderDate = orderDate;
-    }
+	public void setOrderDate(Timestamp orderDate) {
+		this.orderDate = orderDate;
+	}
 
-    public String getPaymentStatus() {
-        return PaymentStatus;
-    }
+	public String getPaymentStatus() {
+		return PaymentStatus;
+	}
 
-    public void setPaymentStatus(String PaymentStatus) {
-        this.PaymentStatus = PaymentStatus;
-    }
+	public void setPaymentStatus(String PaymentStatus) {
+		this.PaymentStatus = PaymentStatus;
+	}
 
-    public String getOrderStatus() {
-        return orderStatus;
-    }
+	public String getOrderStatus() {
+		return orderStatus;
+	}
 
-    public void setOrderStatus(String orderStatus) {
-        this.orderStatus = orderStatus;
-    }
+	public void setOrderStatus(String orderStatus) {
+		this.orderStatus = orderStatus;
+	}
 
-    public String getCustomerId() {
-        return customerId;
-    }
+	public String getCustomerId() {
+		return customerId;
+	}
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
 
-    public String getMethodId() {
-        return methodId;
-    }
+	public String getMethodId() {
+		return methodId;
+	}
 
-    public void setMethodId(String methodId) {
-        this.methodId = methodId;
-    }
+	public void setMethodId(String methodId) {
+		this.methodId = methodId;
+	}
 
-    public String getStaffID() {
-        return StaffID;
-    }
+	public String getStaffID() {
+		return StaffID;
+	}
 
-    public void setStaffID(String StaffID) {
-        this.StaffID = StaffID;
-    }
+	public void setStaffID(String StaffID) {
+		this.StaffID = StaffID;
+	}
 
-   
 }
