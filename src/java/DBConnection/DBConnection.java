dd1b9c8b9d00637f20f166b3b0c6b0b013e73155
Duycld03 <truongduy2003@gmail.com>
2024-02-28 13:34:27 +0700

    before merge

--- src/java/DBConnection/DBConnection.java
+++ src/java/DBConnection/DBConnection.java
@@ -8,15 +8,11 @@ import java.util.logging.Logger;
 
 public class DBConnection {
 
-	private static String server = "localhost";
-	private static String port = "1433";
-	private static String database = "ShoeStoreManagement";
-	private static String user = "sa";
-<<<<<<< HEAD
-	private static String password = "123456";
-=======
-	private static String password = "123";
->>>>>>> 14e52ad79b66cd553e044dd35b5bacf5675a6281
+	private static final String server = "localhost";
+	private static final String port = "1433";
+	private static final String database = "ShoeStoreManagement";
+	private static final String user = "sa";
+	private static final String password = "123";
 
 	public static Connection getConnection() {
 		Connection conn = null;
