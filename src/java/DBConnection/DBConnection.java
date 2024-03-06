149c32969b32955485acfc62547d3a2893d6b516
tiendm2603 <tiendmce170962@fpt.edu.vn>
2024-03-06 16:27:19 +0700

    add cart

--- src/java/DBConnection/DBConnection.java
+++ src/java/DBConnection/DBConnection.java
@@ -8,38 +8,28 @@ import java.util.logging.Logger;
 
 public class DBConnection {
 
-<<<<<<< HEAD
-	private static String server = "localhost";
-	private static String port = "1433";
-	private static String database = "ShoeStoreManagement";
-	private static String user = "sa";
-	private static String password = "123456";
+    private static String server = "localhost";
+    private static String port = "1433";
+    private static String database = "ShoeStoreManagement";
+    private static String user = "sa";
+    private static String password = "123456";
 
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
-=======
-	private static final String server = "localhost";
-	private static final String port = "1433";
-	private static final String database = "ShoeStoreManagement";
-	private static final String user = "sa";
-	private static final String password = "123";
->>>>>>> b86d6d84ba2b95a1613cccae59d776024d26ca60
+            try {
+                conn = DriverManager.getConnection("jdbc:sqlserver://" + server + ":" + port
+                        + ";databaseName=" + database + ";user=" + user + ";password=" + password
+                        + ";encrypt=true;trustServerCertificate=true;");
+            } catch (SQLException ex) {
+                Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
 
-	public static Connection getConnection() {
-		Connection conn = null;
-		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-
-			try {
-				conn = DriverManager.getConnection("jdbc:sqlserver://" + server + ":" + port
-						+ ";databaseName=" + database + ";user=" + user + ";password=" + password
-						+ ";encrypt=true;trustServerCertificate=true;");
-			} catch (SQLException ex) {
-				Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-			}
-		} catch (ClassNotFoundException ex) {
-			Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-		return conn;
-	}
+        return conn;
+    }
 }
