bbdcd79642b8ea604bdf2a4bc225d28d42013696
Duycld03 <truongduy2003@gmail.com>
2024-03-19 13:17:19 +0700

    fix null cookie

--- src/java/DBConnection/DBConnection.java
+++ src/java/DBConnection/DBConnection.java
@@ -10,9 +10,9 @@ public class DBConnection {
 
 	private static String server = "localhost";
 	private static String port = "1433";
-	private static String database = "ShoeStoreManagementVer10";
+	private static String database = "ShoeStoreManagement";
 	private static String user = "sa";
-	private static String password = "12345";
+	private static String password = "123";
 
 	public static Connection getConnection() {
 		Connection conn = null;
